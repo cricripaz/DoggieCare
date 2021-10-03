e0b3e0d741bedfd648d9df5590f60753c83d5d4a
H0OE <rodrigoquiroz308@gmail.com>
2021-10-03 15:11:21 -0400

    Publicaciones, formulario para nuevas publicaciones

---
+++ app/src/main/java/com/backyardigans/doggiecare/data/TemptDataSource.kt
@@ -0,0 +1,17 @@
+package com.backyardigans.doggiecare.data
+
+import com.backyardigans.doggiecare.Model.Feed
+import com.backyardigans.doggiecare.R
+
+object TemptDataSource {
+    val postList = mutableListOf<Feed>(
+        Feed( R.drawable.ic_img_dog,"San Icticolads", "pescado","Salmo-nes","100 anios"),
+        Feed( R.drawable.ic_icon_perrito,"San Icticolas", "pescado","Salmo-nes","100 anios"),
+        Feed( R.drawable.ic_icon_perrito,"San Icticolas", "pescado","Salmo-nes","100 anios"),
+        Feed( R.drawable.ic_icon_perrito,"San Icticolas", "pescado","Salmo-nes","100 anios"),
+        Feed( R.drawable.ic_icon_perrito,"San Icticolas", "pescado","Salmo-nes","100 anios"),
+        Feed( R.drawable.ic_icon_perrito,"San Icticolas", "pescado","Salmo-nes","100 anios"),
+        Feed( R.drawable.ic_icon_perrito,"San Icticolas", "pescado","Salmo-nes","100 anios"),
+        Feed( R.drawable.ic_icon_perrito,"San Icticolas", "pescado","Salmo-nes","100 anios")
+    )
+}
