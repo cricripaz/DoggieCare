971a572de354c633daea662903ff2ca41dc4b17f
Fernanda Martínez <f.l.m.m.0027@gmail.com>
2021-10-03 21:17:32 -0400

    fixed activity profile to fragment

--- app/src/main/java/com/backyardigans/doggiecare/fragments/EditProfileFragment.kt
+++ app/src/main/java/com/backyardigans/doggiecare/fragments/EditProfileFragment.kt
@@ -2,16 +2,26 @@ package com.backyardigans.doggiecare.fragments
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.backyardigans.doggiecare.R
 
 class EditProfileFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.fragment_edit_profile, container, false)
+    override fun  onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                               savedInstanceState: Bundle?): View {
+
+        val view: View = inflater.inflate(
+            R.layout.fragment_edit_profile,
+            container, false
+        )
+        val button = view.findViewById<View>(R.id.button_save) as Button
+        button.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+
+            }
+        })
+        return view
     }
 }
