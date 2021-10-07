e1d7516797bc233f5c99736eabd60f191121a5ac
Fernanda Martínez <f.l.m.m.0027@gmail.com>
2021-10-07 09:00:59 -0400

    detail screen without love buttom

--- app/src/main/java/com/backyardigans/doggiecare/fragments/ProfileFragment.kt
+++ app/src/main/java/com/backyardigans/doggiecare/fragments/ProfileFragment.kt
@@ -9,16 +9,17 @@ import androidx.fragment.app.Fragment
 import com.backyardigans.doggiecare.R
 import android.widget.Button
 import androidx.fragment.app.FragmentTransaction
+import com.bumptech.glide.Glide
 
 
 class ProfileFragment :  Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-
         val view: View = inflater.inflate(
             R.layout.activity_profile_fragment,
             container, false
         )
+
         val button = view.findViewById<View>(R.id.button_editar) as Button
         button.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
