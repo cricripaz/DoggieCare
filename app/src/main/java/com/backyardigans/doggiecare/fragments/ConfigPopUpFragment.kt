478d58645611b76b9049eb2b7bb5a072da5697e8
H0OE <rodrigoquiroz308@gmail.com>
2021-10-16 17:35:44 -0400

    fixes

--- app/src/main/java/com/backyardigans/doggiecare/fragments/ConfigPopUpFragment.kt
+++ app/src/main/java/com/backyardigans/doggiecare/fragments/ConfigPopUpFragment.kt
@@ -1,5 +1,6 @@
 package com.backyardigans.doggiecare.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,12 +8,15 @@ import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.backyardigans.doggiecare.Preferences.UserApplication.Companion.prefs
 import com.backyardigans.doggiecare.R
-import com.backyardigans.doggiecare.databinding.ActivityAddFragmentBinding
+import com.backyardigans.doggiecare.activities.FeedActivity
+import com.backyardigans.doggiecare.activities.LoginActivity
 import com.backyardigans.doggiecare.databinding.FragmentPopupConfigurationsBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class ConfigPopUpFragment : Fragment() {
+class ConfigPopUpFragment : BottomSheetDialogFragment() {
     private var _binding: FragmentPopupConfigurationsBinding?=null
     private val binding get() = _binding!!
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -23,10 +27,14 @@ class ConfigPopUpFragment : Fragment() {
         binding.popupEditarPerfil.setOnClickListener {
             //hacer con navigation
             Toast.makeText(activity, "editar perfil", Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_configPopUpFragment2_to_editProfileFragment)
+            //TODO mejorar el como se muestra
+
         }
 
-        binding.popupSolicitarVerificacion.setOnClickListener {
-            Toast.makeText(activity, "Su solicitud será procesada por nuestro equipo", Toast.LENGTH_SHORT).show()
+        binding.popupSolicitarVerificacion.setOnClickListener{
+            Toast.makeText(activity, "Solicitando verificacion", Toast.LENGTH_SHORT).show()
+
         }
 
         binding.popupCambiarModo.setOnClickListener {
@@ -35,9 +43,13 @@ class ConfigPopUpFragment : Fragment() {
 
         binding.popupSalir.setOnClickListener {
             prefs.erase()
-            Toast.makeText(activity, "Saliendo de la cuenta", Toast.LENGTH_SHORT).show()
-        }
 
+            Toast.makeText(activity, "Saliendo de la cuenta", Toast.LENGTH_SHORT).show()
+            activity?.let{
+                val intent = Intent (it, LoginActivity::class.java)
+                it.startActivity(intent)
+            }
+        }
 
         return binding.root
     }
