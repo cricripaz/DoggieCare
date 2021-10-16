8ef7dbcc0ae9108a4224ac44d4dd1a5a556273bc
H0OE <rodrigoquiroz308@gmail.com>
2021-10-16 17:35:09 -0400

    LogOut

--- app/src/main/java/com/backyardigans/doggiecare/activities/MainActivity.kt
+++ app/src/main/java/com/backyardigans/doggiecare/activities/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.backyardigans.doggiecare.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.backyardigans.doggiecare.Preferences.UserApplication.Companion.prefs
 import com.backyardigans.doggiecare.R
+import com.backyardigans.doggiecare.databinding.ActivityLoginBinding
+import com.backyardigans.doggiecare.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btn.setOnClickListener{
+            prefs.erase()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+
 
     }
 }
