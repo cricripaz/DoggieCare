b53f0568a98a6868c0ca9d259d4034758ed64dc5
H0OE <rodrigoquiroz308@gmail.com>
2021-10-19 07:19:52 -0400

    New to profile

--- app/src/main/java/com/backyardigans/doggiecare/fragments/WelcomeBioFragment.kt
+++ app/src/main/java/com/backyardigans/doggiecare/fragments/WelcomeBioFragment.kt
@@ -4,20 +4,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import com.backyardigans.doggiecare.Model.Profile
 import com.backyardigans.doggiecare.R
 import com.backyardigans.doggiecare.base.StepsBaseFragment
+import com.backyardigans.doggiecare.databinding.ActivityProfileFragmentBinding
+import com.backyardigans.doggiecare.databinding.FragmentWelcomeBioBinding
+import com.backyardigans.doggiecare.viewModel.ProfileViewModel
 
 class WelcomeBioFragment :  StepsBaseFragment() {
+    private val profileViewModel: ProfileViewModel by activityViewModels()
+
+    private var _binding: FragmentWelcomeBioBinding?=null
+    private val binding get() = _binding!!
 
     private lateinit var btListo: View
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_welcome_bio, container, false)
+        _binding = FragmentWelcomeBioBinding.inflate(inflater, container, false)
+
+
+
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -25,6 +38,7 @@ class WelcomeBioFragment :  StepsBaseFragment() {
 
         btListo.setOnClickListener {
             val goToFeed = WelcomeBioFragmentDirections.actionWelcomeBioFragmentToFeedActivity()
+            profileViewModel.actualizar(Profile(null, binding.welcomebio.text.toString()))
             findNavController().navigate(goToFeed)
         }
     }
--- app/src/main/java/com/backyardigans/doggiecare/viewModel/ProfileViewModel.kt
+++ app/src/main/java/com/backyardigans/doggiecare/viewModel/ProfileViewModel.kt
@@ -3,12 +3,22 @@ package com.backyardigans.doggiecare.viewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.backyardigans.doggiecare.Model.Profile
+import com.backyardigans.doggiecare.Preferences.Prefs
+import com.backyardigans.doggiecare.Preferences.UserApplication.Companion.prefs
 
 class ProfileViewModel:ViewModel() {
-    val profileModel = MutableLiveData<Profile>(Profile("Default", "Default"))
+    val profileModel = MutableLiveData<Profile>(Profile(prefs.getUser(), prefs.getBio()))
 
     fun actualizar(profile:Profile){
-        val currentData = profile
-        profileModel.postValue(currentData)
+
+
+        if (profile.bio != null) {
+            prefs.saveBio(profile.bio)
+        }
+        if (profile.nickName != null) {
+            prefs.saveUser(profile.nickName)
+        }
+
+        profileModel.postValue(Profile(prefs.getUser(), prefs.getBio()))
     }
 }
