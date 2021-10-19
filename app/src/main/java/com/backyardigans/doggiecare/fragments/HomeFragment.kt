9a8e95cafc15ddfb1b5e0189d0a91f4762cf03d3
H0OE <rodrigoquiroz308@gmail.com>
2021-10-19 07:25:34 -0400

    binding adds

--- app/src/main/java/com/backyardigans/doggiecare/fragments/HomeFragment.kt
+++ app/src/main/java/com/backyardigans/doggiecare/fragments/HomeFragment.kt
@@ -12,13 +12,19 @@ import androidx.recyclerview.widget.RecyclerView
 import com.backyardigans.doggiecare.adapters.FeedAdapter
 import com.backyardigans.doggiecare.R
 import com.backyardigans.doggiecare.data.TemptDataSource
+import com.backyardigans.doggiecare.databinding.ActivityHomeFragmentBinding
+import com.backyardigans.doggiecare.databinding.FragmentEditProfileBinding
 
 class HomeFragment : Fragment() {
+    private var _binding: ActivityHomeFragmentBinding?=null
+    private val binding get() = _binding!!
     private val feedAdapter = FeedAdapter()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                           savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.activity_home_fragment, container, false)
+        _binding = ActivityHomeFragmentBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
