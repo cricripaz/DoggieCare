e9696355995470b468d89dbc80391c6ade9c3b6a
cricripaz <49013118+cricripaz@users.noreply.github.com>
2021-10-18 23:18:40 -0400

    mejorando el SearchFragment

--- app/src/main/java/com/backyardigans/doggiecare/fragments/SearchFragment.kt
+++ app/src/main/java/com/backyardigans/doggiecare/fragments/SearchFragment.kt
@@ -6,11 +6,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.backyardigans.doggiecare.R
+import com.backyardigans.doggiecare.adapters.FeedAdapter
+import com.backyardigans.doggiecare.data.TemptDataSource
 
 class SearchFragment : Fragment() {
+    private val feedAdapter = FeedAdapter()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.activity_search_fragment, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val recyclerView = view.findViewById<RecyclerView>(R.id.rvFeedSearchFragment)
+        recyclerView.adapter = feedAdapter
+        recyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+
+        LinearSnapHelper().attachToRecyclerView(recyclerView)
+        feedAdapter.addAll(TemptDataSource.postList)
+
+
+        feedAdapter.setOnFeedItemClickListener {
+            val directions = HomeFragmentDirections.actionHomeFragmentToDetailsFragment()
+            findNavController().navigate(directions)
+        }
+    }
+
 }
