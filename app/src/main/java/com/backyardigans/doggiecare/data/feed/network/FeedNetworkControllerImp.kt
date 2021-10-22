15aa3e20e01c677a5bdd7a596d1c4de24f61e60a
cricripaz <49013118+cricripaz@users.noreply.github.com>
2021-10-22 09:13:54 -0400

    Firestore

---
+++ app/src/main/java/com/backyardigans/doggiecare/data/feed/network/FeedNetworkControllerImp.kt
@@ -0,0 +1,21 @@
+package com.backyardigans.doggiecare.data.feed.network
+
+import com.backyardigans.doggiecare.Model.Feed
+import com.backyardigans.doggiecare.data.feed.persistency.FeedPersistencyController
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.tasks.await
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class FeedNetworkControllerImp : FeedNetworkController {
+
+    val db = Firebase.firestore
+
+    override suspend fun getAllPosts(): List<Feed> {
+      val response = db.collection("publicaciones").get().await()
+        return response.toObjects(Feed::class.java)
+    }
+
+
+}
