7fe5a83b692b45dcc117e878b14333e1338d1c41
cricripaz <49013118+cricripaz@users.noreply.github.com>
2021-10-27 09:18:13 -0400

    Firestore adro

--- app/src/main/java/com/backyardigans/doggiecare/fragments/HomeFragment.kt
+++ app/src/main/java/com/backyardigans/doggiecare/fragments/HomeFragment.kt
@@ -5,20 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.RecyclerView
+import com.backyardigans.doggiecare.Model.Feed
 import com.backyardigans.doggiecare.adapters.FeedAdapter
 import com.backyardigans.doggiecare.R
 import com.backyardigans.doggiecare.data.TemptDataSource
 import com.backyardigans.doggiecare.databinding.ActivityHomeFragmentBinding
 import com.backyardigans.doggiecare.databinding.FragmentEditProfileBinding
+import com.backyardigans.doggiecare.viewModel.FeedViewModel
+
 
 class HomeFragment : Fragment() {
+
     private var _binding: ActivityHomeFragmentBinding?=null
     private val binding get() = _binding!!
     private val feedAdapter = FeedAdapter()
+    private val feedModel : FeedViewModel by activityViewModels()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                           savedInstanceState: Bundle?): View {
@@ -33,7 +39,13 @@ class HomeFragment : Fragment() {
         recyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
 
         LinearSnapHelper().attachToRecyclerView(recyclerView)
-        feedAdapter.addAll(TemptDataSource.postList)
+
+        feedModel.updatePost()
+        feedModel.feedList.observe(viewLifecycleOwner , {
+            feedAdapter.addAll(it as MutableList<Feed>)
+        }
+        )
+
 
 
         feedAdapter.setOnFeedItemClickListener {
@@ -41,4 +53,6 @@ class HomeFragment : Fragment() {
             findNavController().navigate(directions)
         }
     }
+
+
 }
