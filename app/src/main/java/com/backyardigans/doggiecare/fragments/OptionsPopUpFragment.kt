e0be697f5df0d9964603d8b8aafa3a40635b199c
Fernanda Martínez <f.l.m.m.0027@gmail.com>
2021-11-01 22:57:16 -0400

    add totally functional for images + gallery + smart options with actions

--- app/src/main/java/com/backyardigans/doggiecare/fragments/OptionsPopUpFragment.kt
+++ app/src/main/java/com/backyardigans/doggiecare/fragments/OptionsPopUpFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
 import androidx.navigation.fragment.findNavController
 import com.backyardigans.doggiecare.R
 import com.backyardigans.doggiecare.databinding.FragmentPopupOptionsBinding
@@ -21,8 +23,8 @@ class OptionsPopUpFragment : BottomSheetDialogFragment() {
         _binding = FragmentPopupOptionsBinding.inflate(inflater, container, false)
 
         if (arguments?.getString("previous").equals("post")) {
-            binding.popupoptions1.text=requireContext().getString(R.string.editar_publicacion)
-            binding.popupoptions2.text= requireContext().getString(R.string.eliminar_publicacion)
+            binding.popupoptions1.text = requireContext().getString(R.string.editar_publicacion)
+            binding.popupoptions2.text = requireContext().getString(R.string.eliminar_publicacion)
 
             binding.popupoptions1.setOnClickListener {
                 findNavController().navigate(R.id.action_optionsPopUpFragment_to_addFragment)
@@ -35,18 +37,37 @@ class OptionsPopUpFragment : BottomSheetDialogFragment() {
             }
 
 
-        } else if (arguments?.getString("previous").equals("add") or arguments?.getString("previous").equals("editProfile") ) {
-            binding.popupoptions1.text=requireContext().getString(R.string.camara)
-            binding.popupoptions2.text= requireContext().getString(R.string.galeria)
-                //todo if camara y galeria
-        } else if (arguments?.getString("previous").equals("block") or arguments?.getString("previous").equals("report") or arguments?.getString("previous").equals("verification")) {
-            binding.popupoptions1.text=requireContext().getString(R.string.proseguir)
-            binding.popupoptions2.text= requireContext().getString(R.string.abortar)
-                //todo bloquear verificar y reportar
+        } else if (arguments?.getString("previous")
+                .equals("add") or arguments?.getString("previous").equals("editProfile")
+        ) {
+            binding.popupoptions1.text = requireContext().getString(R.string.camara)
+            binding.popupoptions2.text = requireContext().getString(R.string.galeria)
+
+            binding.popupoptions1.setOnClickListener {
+                setFragmentResult("requestKey", bundleOf("camorgal" to "cam"))
+                findNavController().popBackStack()
+            }
+
+            binding.popupoptions2.setOnClickListener {
+                setFragmentResult("requestKey", bundleOf("camorgal" to "gal"))
+                findNavController().popBackStack()
+            }
+        } else if (arguments?.getString("previous")
+                .equals("block") or arguments?.getString("previous")
+                .equals("report") or arguments?.getString("previous").equals("verification")
+        ) {
+            binding.popupoptions1.text = requireContext().getString(R.string.proseguir)
+            binding.popupoptions2.text = requireContext().getString(R.string.abortar)
+
+            binding.popupoptions1.setOnClickListener {
+                Toast.makeText(activity, "Su solicitud ha sido enviada", Toast.LENGTH_SHORT).show()
+                //todo bloquear abortar verificar
+            }
+
+            binding.popupoptions2.setOnClickListener {
+                findNavController().popBackStack()
+            }
         }
-
-
-
 
         return binding.root
     }
