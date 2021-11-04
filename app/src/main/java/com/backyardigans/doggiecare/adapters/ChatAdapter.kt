50f04e252933f299938816f8fa5887c3a1e56d84
kpomier777 <kevinpomier.kp@gmail.com>
2021-11-04 06:50:41 -0400

    Click on Item RecyclerView

--- app/src/main/java/com/backyardigans/doggiecare/adapters/ChatAdapter.kt
+++ app/src/main/java/com/backyardigans/doggiecare/adapters/ChatAdapter.kt
@@ -1,6 +1,7 @@
 package com.backyardigans.doggiecare.adapters
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.ColorSpace
 import android.text.Layout
 import android.view.Display
@@ -13,6 +14,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.backyardigans.doggiecare.Model.Chat
 import com.backyardigans.doggiecare.R
+import com.backyardigans.doggiecare.activities.MessageActivity
 import com.backyardigans.doggiecare.databinding.ChatModelBinding
 
 class ChatAdapter(): RecyclerView.Adapter<ChatAdapter.ViewHolder>() {
@@ -49,13 +51,23 @@ class ChatAdapter(): RecyclerView.Adapter<ChatAdapter.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: ChatAdapter.ViewHolder, position: Int) {
-        holder.bind(elementList[position])
-    }
+
+            holder.bind(elementList[position])
+            holder.itemView.setOnClickListener{ v ->
+                val intent = Intent(v.context, MessageActivity::class.java)
+                v.context.startActivity(intent)
+            }
+        }
 
     override fun getItemCount(): Int {
+
         return elementList.size
     }
 
 
-
 }
+
+
+
+
+
